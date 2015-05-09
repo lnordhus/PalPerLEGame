58b06122988fe9134729de0dbcc1e571191df9ee
Lars Espen Nordhus <lars.e.nordhus@gmail.com>
2015-05-09 23:44:59 +0200

    added more files

---
+++ Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+
+	void FixedUpdate(){
+		var moveHorizontal = Input.GetAxis ("Horizontal");
+		var moveVertical = Input.GetAxis ("Vertical");
+		var movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "Pick Up")
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+}
