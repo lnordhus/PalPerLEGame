ffb6a4f69bb5abcaec20d233dc02bd9c32672bc6
PerErikNor <per.e.nordhus@gmail.com>
2015-05-19 21:10:40 +0200

    Check in

--- Test/Assets/Scripts/PlayerController.cs
+++ Test/Assets/Scripts/PlayerController.cs
@@ -22,68 +22,73 @@ public class PlayerController : MonoBehaviour {
 		rb = GetComponent<Rigidbody> ();
 		dist = 0.6f;
 		floorMask = LayerMask.GetMask ("Terrain");
+		Vector3 walkVector = new Vector3 (0, 0, 0);
 	}
 
 	void FixedUpdate()
 	{
 
-		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
-		float moveVertical = Input.GetAxisRaw ("Vertical");
+		//float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		//float moveVertical = Input.GetAxisRaw ("Vertical");
 
-		Vector3 down = new Vector3 (0, -1, 0);
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		//Vector3 down = new Vector3 (0, -1, 0);
+		//Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
 		//lagrer bevegelse hvis i luften
-		if (Physics.Raycast (transform.position, down, out hit, dist) == true) 
-		{
-			horizontalSpeed = moveHorizontal;
-			verticalSpeed = moveVertical;
-		}
+		//if (Physics.Raycast (transform.position, down, out hit, dist) == true) 
+		//{
+		//	horizontalSpeed = moveHorizontal;
+		//	verticalSpeed = moveVertical;
+		//}
 
 		//hopper
-		if (Input.GetKey ("space") && Physics.Raycast(transform.position,down,out hit,dist) == true)
-			{ 
-
-			rb.velocity = new Vector3 (0 , jump , 0);
-			}
+		//if (Input.GetKey ("space") && Physics.Raycast(transform.position,down,out hit,dist) == true)
+		//{ 
+		//
+		//	rb.velocity = new Vector3 (0 , jump , 0);
+		//}
 		//Hvis hopper så fortset x og z bevegelse
-		if (Physics.Raycast(transform.position,down,out hit,dist) == false)
-			{
-			movement = new Vector3 (0,0,0);
-			movement = new Vector3 (horizontalSpeed,0,verticalSpeed);
+		//if (Physics.Raycast(transform.position,down,out hit,dist) == false)
+		//	{
+		//	movement = new Vector3 (0,0,0);
+		//	movement = new Vector3 (horizontalSpeed,0,verticalSpeed);
+		//
+		//	}
 
-			}
-
-		//beveger player
-		rb.MovePosition (rb.position + (movement.normalized * speed * Time.deltaTime));
-
-		//roterer figur
-		if (moveHorizontal != 0 || moveVertical != 0) {
-			Quaternion retning = Quaternion.LookRotation (movement);
-			rb.MoveRotation (retning);
-		}
 
 		//beregner fallhastighet
 		fallSpeed = rb.velocity.y;
 
 		//finner hvor du klikker venstre musetast på terrain og lager en boks
-		if (Input.GetMouseButtonDown (0)) 
-		{
+		if (Input.GetMouseButtonDown (0)) {
 			Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit floorHit;
-			if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) 
-			{
+			if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
 				Vector3 playerToMouse = floorHit.point - transform.position;
 				playerToMouse.y = 0f;
-				GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 				cube.transform.position = playerToMouse;
-				Destroy (cube,1);
-				Vector3 walkVector = rb.position - playerToMouse;
+				cube.tag = "Pick Up";
+				Destroy (cube, 5);
+				walkVector = rb.position + playerToMouse;
+				print ("safas");
 			}
-			 
-			//Destroy(other);
+
+		} 
+		else 
+		{
+			walkVector = new Vector3 (0f, 0f, 0f);
 		}
-		Vector3 walkVector = rb.position - 
+
+		
+		//roterer figur
+		if (walkVector != rb.position) {
+			Quaternion retning = Quaternion.LookRotation (walkVector);
+			rb.MoveRotation (retning);
+		}
+		
+		//beveger player
+		rb.MovePosition (rb.position + (walkVector.normalized * speed * Time.deltaTime));
 	}
 
 	void OnGUI()
